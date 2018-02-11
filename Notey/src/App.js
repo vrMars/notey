525060708be2dd5b0bdcc93c013aa9d66877ee6e
vrMars <vrmars98@gmail.com>
2018-02-11 14:59:57 -0500

    playing with md editors

--- Notey/src/App.js
+++ Notey/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import Editor, { ReactMdeCommands } from 'react-mde';
+import * as ReactMde from 'react-mde';
+import { Markdown } from 'react-showdown';
+import 'simplemde/dist/simplemde.min.css';
+
+
 import 'react-mde/lib/styles/css/react-mde-preview.css';
 import 'react-mde/lib/styles/css/react-mde-textarea.css';
 import 'react-mde/lib/styles/css/react-mde-toolbar.css';
@@ -8,30 +12,40 @@ import '../node_modules/font-awesome/css/font-awesome.css';
 import logo from './logo.svg';
 import './App.css';
 
+var Showdown = require('showdown');
+var SimpleMDE = require('react-simplemde-editor');
+
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { code: '# Today', selection: null };
+		this.state = { code: '', oldCode: '', selection: null };
 	}
 
-	updateCode(newCode) {
-		this.setState({ code: newCode });
+	handleChange(value) {
+		this.setState({
+			code: value
+		});
 	}
-
+	// //ReactMdeCommands.getDefaultCommands()
+	// <div
+	//   ref="editArea"
+	//   contentEditable={true}
+	//   className="editArea"
+	//   dangerouslySetInnerHTML={{ __html: this.state.code }}
+	//   onKeyPress={this.updateCode.bind(this)}
+	// />
 	render() {
-		console.log(this.state.code);
+		console.log('current code: ' + this.state.code);
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Notey</h1>
 				</header>
-				<div className="Editor">
-					<Editor
-						value={this.state.code}
-						onChange={this.updateCode.bind(this)}
-						commands={ReactMdeCommands.getDefaultCommands()}
-					/>
-				</div>
+				<SimpleMDE
+					className="test"
+					initialValue={"Type here!"}
+					onChange={this.handleChange.bind(this)}
+				/>
 			</div>
 		);
 	}
