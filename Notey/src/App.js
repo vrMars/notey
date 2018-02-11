b4d86b288fc3c69add9dea06dc68d8d8743ec430
vrMars <vrmars98@gmail.com>
2018-02-10 22:31:12 -0500

    basic md editor

--- Notey/src/App.js
+++ Notey/src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import Editor, { ReactMdeCommands } from 'react-mde';
+import 'react-mde/lib/styles/css/react-mde-preview.css';
+import 'react-mde/lib/styles/css/react-mde-textarea.css';
+import 'react-mde/lib/styles/css/react-mde-toolbar.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload..
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { code: '# Today', selection: null };
+	}
+
+	updateCode(newCode) {
+		this.setState({ code: newCode });
+	}
+
+	render() {
+		console.log(this.state.code);
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Notey</h1>
+				</header>
+				<div className="Editor">
+					<Editor
+						value={this.state.code}
+						onChange={this.updateCode.bind(this)}
+						commands={ReactMdeCommands.getDefaultCommands()}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
