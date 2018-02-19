6f55ee3070b648c95830acdeb8aaba452176ff1f
vrMars <vrmars98@gmail.com>
2018-02-19 17:37:08 -0500

    vh adjustments

--- Notey/src/App.js
+++ Notey/src/App.js
@@ -3,6 +3,7 @@ import firebase from './fire.js';
 
 import logo from './logo.svg';
 import Editor from './screens/Editor/Editor.js'
+
 class App extends Component {
 	constructor(props) {
 		super(props);
