7ec737b8b5f5b4be7c061756471ec1ce124fbba1
vrMars <vrmars98@gmail.com>
2018-04-19 23:08:11 -0400

    basic non dynamic array

--- Notey/src/screens/Notes/Notes.js
+++ Notey/src/screens/Notes/Notes.js
@@ -4,6 +4,11 @@ import { auth } from '../../firebase/';
 import { fire } from '../../firebase/fire.js';
 import { ToastContainer, toast } from 'react-toastify';
 import * as routes from '../../route';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import img from './img.jpg';
 import './Notes.css';
 
 // Main app
@@ -18,6 +23,22 @@ const INITIAL_STATE = {
     password: '',
     iterator: 0,
     error: null,
+};
+
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        display: 'flex',
+        flexWrap: 'nowrap',
+        overflowX: 'auto',
+    },
+    titleStyle: {
+        color: 'rgb(255, 255, 255)',
+    },
 };
 
 // Main app
@@ -46,12 +67,74 @@ class Notes extends React.Component {
     render() {
         // const for React CSS transition declaration
 
+        const tilesData = [
+            {
+                img: 'img.jpg',
+                title: 'Breakfast',
+                author: 'jill111',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Tasty burger',
+                author: 'pashminu',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Camera',
+                author: 'Danson67',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Morning',
+                author: 'fancycrave1',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Hats',
+                author: 'Hans',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Honey',
+                author: 'fancycravel',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Vegetables',
+                author: 'jill111',
+            },
+            {
+                img: 'img.jpg',
+                title: 'Water plant',
+                author: 'BkrmadtyaKarki',
+            },
+        ];
+
         return (
             <div>
                 <ToastContainer/>
 
-                <div className={"background"}/>
-            </div>
+                <MuiThemeProvider>
+                <div style={styles.root}>
+                    <GridList style={styles.gridList} cols={0} padding={20} cellHeight={250}>
+                        {tilesData.map((tile) => (
+                            <GridTile
+                                key={tile.img}
+                                title={tile.title}
+                                actionIcon={<IconButton><StarBorder color="rgb(255, 255, 255)" /></IconButton>}
+                                titleStyle={styles.titleStyle}
+                                titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                            >
+                                <img src={img} />
+                            </GridTile>
+                        ))}
+                    </GridList>
+                </div>
+
+                </MuiThemeProvider>
+
+
+                </div>
         );
     }
 }
